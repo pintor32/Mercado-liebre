cc7a40900db268995bbc00ba4a08fd44187db092
Leonardo Pintor <jpintormore@uniminuto.edu.co>
2021-10-15 09:35:52 -0500

    creando repo

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.listen(port, ()=>console.log('♥ running with love'))
